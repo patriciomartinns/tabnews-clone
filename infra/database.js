52a37457c24484ba9835523bd05f1087b8d379f3
Patrício Martins <eupatriciomartins@gmail.com>
2025-03-20 17:09:11 -0300

    fix: improve error handling in database query function

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,14 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-
-  return result;
+  try {
+    await client.connect();
+    return await client.query(queryObject);
+  } catch (error) {
+    console.error("Error executing query", error);
+  } finally {
+    await client.end();
+  }
 }
 
 const database = {
