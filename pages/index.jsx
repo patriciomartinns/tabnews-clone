674c7d348ee0fa5c0a0fbec362ac442af588ab7b
Patrício Martins <eupatriciomartins@gmail.com>
2025-03-20 11:42:56 -0300

    chore: update styles and configuration for Prettier and VSCode

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,33 @@
 export default function IndexPage() {
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>Filipe faz um vídeo retrô lembrando os velhos tempos.</h1>
-      <p style={styles.description}>Camisa <strong>branca</strong> e fundo <strong>branco</strong> hahhahah ❤️</p>
+      <h1 style={styles.title}>
+        Filipe faz um vídeo retrô lembrando os velhos tempos.
+      </h1>
+      <p style={styles.description}>
+        Camisa <strong>branca</strong> e fundo <strong>branco</strong> hahhahah
+        ❤️
+      </p>
     </div>
   );
 }
 
 const styles = {
   container: {
-    fontFamily: 'sans-serif',
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: '1rem',
+    fontFamily: "sans-serif",
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "1rem",
   },
   title: {
-    fontSize: '24px',
-    fontWeight: 'bold',
+    fontSize: "24px",
+    fontWeight: "bold",
   },
   description: {
-    fontSize: '16px',
+    fontSize: "16px",
   },
 };
