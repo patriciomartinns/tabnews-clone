0da6eb3a4df7c80f5a02de9d7cb41d84535407d8
Patrício Martins <eupatriciomartins@gmail.com>
2025-03-21 16:43:31 -0300

    feat: enhance migration endpoint to support POST method and add integration tests

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,18 +2,22 @@ import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 
 export default async function migrations(request, response) {
-  if (request.method !== "GET") {
-    throw new Error("Method not allowed");
+  if (!["GET", "POST"].includes(request.method)) {
+    return response.status(405).end();
   }
 
   const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
+    dryRun: request.method === "GET",
     dir: resolve("infra", "migrations"),
     direction: "up",
     migrationsTable: "pgmigrations",
     verbose: true,
   });
+
+  if (request.method === "POST" && migrations.length > 0) {
+    return response.status(201).json(migrations);
+  }
 
   return response.status(200).json(migrations);
 }
