bfad81052ccbcaa47816beb1bcb3e65f05a47c55
Patrício Martins <eupatriciomartins@gmail.com>
2025-03-26 02:21:31 -0300

    test: restructure migration and status tests for improved clarity and organization

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,17 +4,21 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should returns 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-  const responseBody = await response.json();
-  const updatedAt = new Date(
-    responseBody.dependencies.updated_at,
-  ).toISOString();
+      const responseBody = await response.json();
+      const updatedAt = new Date(
+        responseBody.dependencies.updated_at,
+      ).toISOString();
 
-  expect(responseBody.dependencies.updated_at).toEqual(updatedAt);
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(response.status).toBe(200);
+      expect(responseBody.dependencies.updated_at).toEqual(updatedAt);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
