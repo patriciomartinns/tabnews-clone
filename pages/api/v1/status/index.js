1a01b8217d85d31a5ee3d54abca6acdb352fb773
Patrício Martins <eupatriciomartins@gmail.com>
2025-03-26 19:59:28 -0300

    feat: implement custom InternalServerError class and handle errors in status API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,25 +1,36 @@
 import database from "infra/database";
+import { InternalServerError } from "infra/errors";
 
 export default async function status(_, response) {
-  const databaseVersion = await database.query("SHOW server_version;");
-  const databaseMaxConnections = await database.query("SHOW max_connections;");
-  const databaseCurrentConnections = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [process.env.POSTGRES_DB],
-  });
-  const databaseCurrentConnectionsCount =
-    databaseCurrentConnections.rows[0].count;
+  try {
+    const databaseVersion = await database.query("SHOW server_version;");
+    const databaseMaxConnections = await database.query(
+      "SHOW max_connections;",
+    );
+    const databaseCurrentConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [process.env.POSTGRES_DB],
+    });
+    const databaseCurrentConnectionsCount =
+      databaseCurrentConnections.rows[0].count;
 
-  response.status(200).json({
-    dependencies: {
-      updated_at: new Date().toISOString(),
-      database: {
-        version: databaseVersion.rows[0].server_version,
-        max_connections: parseInt(
-          databaseMaxConnections.rows[0].max_connections,
-        ),
-        opened_connections: databaseCurrentConnectionsCount,
+    response.status(200).json({
+      dependencies: {
+        updated_at: new Date().toISOString(),
+        database: {
+          version: databaseVersion.rows[0].server_version,
+          max_connections: parseInt(
+            databaseMaxConnections.rows[0].max_connections,
+          ),
+          opened_connections: databaseCurrentConnectionsCount,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    response.status(500).json(publicErrorObject);
+  }
 }
