6455fd23b859148366c45b9280a61884efd5d934
Patrício Martins <eupatriciomartins@gmail.com>
2025-03-27 02:23:14 -0300

    feat: create users table in database migration

--- infra/migrations/1742520372608_create-users.js
+++ infra/migrations/1742520372608_create-users.js
@@ -1,5 +1,26 @@
 exports.shorthands = undefined;
 
-exports.up = () => {};
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    username: { type: "varchar(30)", notNull: true, unique: true },
+    email: { type: "varchar(254)", notNull: true, unique: true },
+    password: { type: "varchar(72)", notNull: true },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+  });
+};
 
-exports.down = () => {};
+exports.down = false;
